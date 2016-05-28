3d2c1511008459a0de483fe551792059fe35698c
tviokrk <tviokrk@gmail.com>
2016-05-28 15:57:45 +0200

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,24 @@
+<?php
+
+// A list of permitted file extensions
+$allowed = array('png', 'jpg', 'gif','zip');
+
+if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
+
+	$extension = pathinfo($_FILES['upl']['name'], PATHINFO_EXTENSION);
+
+	if(!in_array(strtolower($extension), $allowed)){
+		echo 'ERROR';
+		echo '{"status":"error"}';
+		exit;
+	}
+
+	if(move_uploaded_file($_FILES['upl']['tmp_name'], '../uploads'.$_FILES['upl']['name'])){
+		echo 'SUKCES';
+		echo '{"status":"success"}';
+		exit;
+	}
+}
+
+echo '{"status":"error"}';
+exit;
