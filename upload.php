1daab57668fde0c71ce35d0188161d1461ac141c
tviokrk <tviokrk@gmail.com>
2016-05-28 17:31:16 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,8 +1,16 @@
 <?php
-
+//include the S3 class              
+if (!class_exists('S3'))require_once('S3.php');
+ 
+//AWS access info
+if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAIXPGZAUCI7HCXYFA');
+if (!defined('awsSecretKey')) define('awsSecretKey', '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');
+ 
+//instantiate the class
+$s3 = new S3(awsAccessKey, awsSecretKey);
+////////////////////////////////////////////
 // A list of permitted file extensions
 $allowed = array('png', 'jpg', 'gif','zip');
-echo "jest PHP";
 if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
 
 	$extension = pathinfo($_FILES['upl']['name'], PATHINFO_EXTENSION);
@@ -13,7 +21,7 @@ if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
 		exit;
 	}
 
-	if(move_uploaded_file($_FILES['upl']['tmp_name'], './uploads/'.$_FILES['upl']['name'])){
+	if($s3->putObjectFile($_FILES['upl']['tmp_name'], "160689-michalo", $_FILES['upl']['name'], , S3::ACL_PUBLIC_READ)){
 		echo 'SUKCES';
 		echo '{"status":"success"}';
 		exit;
