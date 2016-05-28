72702c998f4aed7b4cd938589c0e0a364523ffe7
tviokrk <tviokrk@gmail.com>
2016-05-28 14:02:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <head>
 <meta http-equiv="Content-type" content="text/html; charset=iso-8859-2" />
-<meta name="author" content="Michał Oprych" />
+<meta name="author" content="MichaÂł Oprych" />
 <title>Wirtualizacja</title>
 
 <style type="text/css">
@@ -23,11 +23,11 @@ background-color: #99CCFF;
 <center>
 <table border=1 rules=all width=800>
 <tr>
- <td width=133><center><a href="pobierz.php" target="ramka"><img src="ikony/pobierz.png" width="80" height="80"></a><br>Wylij dane</center>
+ <td width=133><center><a href="uploader/upload.php" target="ramka"><img src="ikony/pobierz.png" width="80" height="80"></a><br>WyĹlij obrazki</center>
  <td width=133><center><a href="transformacja.php" target="ramka"><img src="ikony/transformuj.png" width="80" height="80"></a><br>Przygotuj galerie</center>
- <td width=133><center><a href="gallery.php" target="ramka"><img src="ikony/wyslij.png" width="80" height="80"></a><br>Pokaż mojš galerię</center>
+ <td width=133><center><a href="gallery.php" target="ramka"><img src="ikony/wyslij.png" width="80" height="80"></a><br>PokaÂż mojÂš galeriĂŞ</center>
  <td width=133><center><a href="etl.php" target="ramka"><img src="ikony/etl.png" width="80" height="80"></a><br>TEST</center>
- <td width=133><center><a href="glowna.php" target="ramka"><img src="ikony/powrot.png" width="80" height="80"></a><br>Wróć na stronę główną</center>
+ <td width=133><center><a href="glowna.php" target="ramka"><img src="ikony/powrot.png" width="80" height="80"></a><br>WrĂłĂŚ na stronĂŞ gÂłĂłwnÂą</center>
 <tr>
  <td colspan=6><iframe src="glowna.php" name="ramka" width="800" height="400" frameborder="0"></iframe> 
 </table>
