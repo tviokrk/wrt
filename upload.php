4fd5f9b0697d60bf373c78636719b11175a413f8
tviokrk <tviokrk@gmail.com>
2016-05-28 16:00:41 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -13,7 +13,7 @@ if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
 		exit;
 	}
 
-	if(move_uploaded_file($_FILES['upl']['tmp_name'], '../uploads'.$_FILES['upl']['name'])){
+	if(move_uploaded_file($_FILES['upl']['tmp_name'], './uploads'.$_FILES['upl']['name'])){
 		echo 'SUKCES';
 		echo '{"status":"success"}';
 		exit;
