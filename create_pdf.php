5062713499b95a8db1f6ca7875436b1d10afee11
tviokrk <tviokrk@gmail.com>
2016-06-04 11:46:03 +0200

    Update create_pdf.php

--- create_pdf.php
+++ create_pdf.php
@@ -5,7 +5,6 @@ use Aws\S3\S3Client;
 // Instantiate an Amazon S3 client.
 $objects = $s3Client->getIterator('ListObjects', array(
     'Bucket' => '160689-michalo',
-    'Prefix' => 'files/'
 ));
 foreach ($objects as $object) {
     echo $object['Key'] . "\n";
