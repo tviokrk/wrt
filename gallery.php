3cc8ae9ee31b0a28bb240a43ed8f6a39a994c463
tviokrk <tviokrk@gmail.com>
2016-06-04 13:57:35 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -42,7 +42,7 @@ while(!feof($file))
 {
  $link = echo fgets($file);
  echo $link;
- echo '<li><a href="'.$link.'" class="highslide" onclick="return hs.expand(this)"><img width="200" height="133" src="'$link'" /></a></li>';
+// echo '<li><a href="'.$link.'" class="highslide" onclick="return hs.expand(this)"><img width="200" height="133" src="'$link'" /></a></li>';
 }
 fclose($file);
 
