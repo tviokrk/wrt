ff2a5692f7f65324d10f0410720ffd010ba25b89
tviokrk <tviokrk@gmail.com>
2016-06-04 11:47:24 +0200

    Update create_pdf.php

--- create_pdf.php
+++ create_pdf.php
@@ -3,6 +3,11 @@
 require 'vendor/autoload.php';
 use Aws\S3\S3Client;
 // Instantiate an Amazon S3 client.
+$s3 = new S3Client([
+    'version' => 'latest',
+    'region'  => 'us-west-2'
+]);
+//List files in Bucket
 $objects = $s3Client->getIterator('ListObjects', array(
     'Bucket' => '160689-michalo',
 ));
