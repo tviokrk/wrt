eaf6f8aea2a0be5ce074ed60af23ffd1a5f5ea6f
tviokrk <tviokrk@gmail.com>
2016-06-04 11:53:33 +0200

    Update create_pdf.php

--- create_pdf.php
+++ create_pdf.php
@@ -2,6 +2,13 @@
 // Require the Composer autoloader.
 require 'vendor/autoload.php';
 use Aws\S3\S3Client;
+use Aws\Common\Aws;
+
+// Create a service builder using a configuration file
+$aws = Aws::factory('./composer.json');
+
+// Get the client from the builder by namespace
+$client = $aws->get('S3');
 // Instantiate an Amazon S3 client.
 $s3 = new S3Client([
     'version' => 'latest',
