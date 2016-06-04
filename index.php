6c1cbc67753dc9e00e85a4ba9913130898d84052
tviokrk <tviokrk@gmail.com>
2016-06-04 11:39:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ background-color: #99CCFF;
 <table border=1 rules=all width=800>
 <tr>
  <td width=133><center><a href="upload.html" target="ramka"><img src="ikony/pobierz.png" width="80" height="80"></a><br>Wyślij obrazki</center>
- <td width=133><center><a href="transformacja.php" target="ramka"><img src="ikony/transformuj.png" width="80" height="80"></a><br>Przygotuj galerie</center>
+ <td width=133><center><a href="create_pdf.php" target="ramka"><img src="ikony/transformuj.png" width="80" height="80"></a><br>Twórz PDF</center>
  <td width=133><center><a href="gallery.php" target="ramka"><img src="ikony/wyslij.png" width="80" height="80"></a><br>Poka¿ moj¹ galeriê</center>
  <td width=133><center><a href="etl.php" target="ramka"><img src="ikony/etl.png" width="80" height="80"></a><br>TEST</center>
  <td width=133><center><a href="glowna.php" target="ramka"><img src="ikony/powrot.png" width="80" height="80"></a><br>Wróæ na stronê g³ówn±</center>
