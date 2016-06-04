530b4d98723788045f3b90accc63ab3ac265472d
tviokrk <tviokrk@gmail.com>
2016-06-04 15:03:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ background-color: #99CCFF;
  <td width="20%"><center><a href="etl.php" target="ramka"><img src="ikony/etl.png" width="80" height="80"></a><br>TEST</center>
  <td width="20%"><center><a href="glowna.php" target="ramka"><img src="ikony/powrot.png" width="80" height="80"></a><br>Wróć na stronę główną</center>
 <tr>
- <td colspan=6><iframe src="glowna.php" name="ramka" width="100%" height="100%" frameborder="0"></iframe> 
+ <td colspan=6><iframe src="glowna.php" name="ramka" width="100%" height="700" frameborder="0"></iframe> 
 </table>
 </center>
 
