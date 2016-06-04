46429ad7a72df8ea1e855c2f5c16b438c79953e4
tviokrk <tviokrk@gmail.com>
2016-06-04 13:55:30 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -41,6 +41,7 @@ $file = fopen($file_name, "r");
 while(!feof($file))
 {
  $link = fgets($file);
+ echo $link;
  echo '<li><a href="'.$link.'" class="highslide" onclick="return hs.expand(this)"><img width="200" height="133" src="'$link'" /></a></li>';
 }
 fclose($file);
