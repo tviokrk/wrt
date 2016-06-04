11d229ddd80ed77adf14dc01a40d41c7473f53dc
tviokrk <tviokrk@gmail.com>
2016-06-04 13:53:55 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -40,8 +40,8 @@ $file_name = "./upload/lista.txt";
 $file = fopen($file_name, "r");
 while(!feof($file))
 {
- //echo fgets($file). "<br />";
- echo '<li><a href="'.fgets($file).'" class="highslide" onclick="return hs.expand(this)"><img width="200" height="133" src="'.fgets($file)'" /></a></li>';
+ $link = fgets($file);
+ echo '<li><a href="'.$link.'" class="highslide" onclick="return hs.expand(this)"><img width="200" height="133" src="'$link'" /></a></li>';
 }
 fclose($file);
 
