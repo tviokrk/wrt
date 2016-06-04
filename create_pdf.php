78639c3c6af3332600e2f15662556203c825daa2
tviokrk <tviokrk@gmail.com>
2016-06-04 12:13:16 +0200

    Update create_pdf.php

--- create_pdf.php
+++ create_pdf.php
@@ -1,22 +1,22 @@
 <?php
-// Require the Composer autoloader.
-require 'vendor/autoload.php';
 use Aws\S3\S3Client;
 
+require 'vendor/autoload.php';
 
-
-// Instantiate an Amazon S3 client.
-$s3Client = new S3Client([
-    'version' => 'latest',
-    'region'  => 'us-west-2'
-]);
-//List files in Bucket
-$iterator = $s3Client->getIterator('ListObjects', array(
-    'Bucket' => '160689-michalo'
+$s3 = S3Client::factory(array(
+    'key'    => 'AKIAIXPGZAUCI7HCXYFA',
+    'secret' => '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',
+    'region' => 'us-west-2'
 ));
 
-foreach ($iterator as $object) {
-    echo $object['Key'] . "\n";
-}
+$bucket = '160689-michalo';
 
+$objects = $s3->getIterator('ListObjects', array(
+    "Bucket" => $bucket,
+
+));
+
+foreach ($objects as $object) {
+    echo $object['Key'] . "<br>";
+}
 exit;
