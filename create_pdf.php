9a9825e764121968ce253d62e4a3aa982ff3c65f
tviokrk <tviokrk@gmail.com>
2016-06-05 23:14:56 +0200

    Update create_pdf.php

--- create_pdf.php
+++ create_pdf.php
@@ -31,8 +31,10 @@ try {
 } catch (S3Exception $e) {
     echo $e->getMessage() . "\n";
 }
-$pdf->Image('https://s3-us-west-2.amazonaws.com/160689-michalo/album_error.png');
-//$pdf->Cell(0,10,"welcome");
+$pdf->Cell(0,10,$object['Key']. "\n");
+//$pdf->Image('https://s3-us-west-2.amazonaws.com/160689-michalo/album_error.png');
+$image1='https://s3-us-west-2.amazonaws.com/160689-michalo/album_error.png';
+$pdf->Cell( 40, 40, $pdf->Image($image1, $pdf->GetX(), $pdf->GetY(), 33.78), 0, 0, 'L', false );
 $filename="/var/www/html/upload/test.pdf";
 $pdf->Output($filename,'F');
 
