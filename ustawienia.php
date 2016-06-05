ee0984ffddc996bb76e87465975407b124bc0917
tviokrk <tviokrk@gmail.com>
2016-06-05 23:52:15 +0200

    Update ustawienia.php

--- ustawienia.php
+++ ustawienia.php
@@ -5,19 +5,12 @@
         <meta charset="utf-8">
     </head>
     <body>
-		<center><br><h5>Plik o nazwie <u>"ceneo"</u> z rozszerzeniem .txt , każda linia powinna zawierać jeden kod towaru.<br>Kod towaru musi mieć min. 1 opinię !</h5>
-        <form action="ustawienia.php" method="post">
+		<center><br><h5>Wprowadź nazwę albumu, który chcesz usunąć</h5>
+        <form action="usun_plik.php" method="post">
     		Nazwa albumu:<input type="text" name="plik_z_albumem" /><br />
     	<input type="submit" name="submit" value="Usuń!" />
 	</form>
 <br>
-
-<?php
-$file = './upload/'.$_POST['plik_z_albumem']; 
-unlink($file);
-
-print("Plik usunięty !");
-?>
 </center>
     </body>
 </html>
