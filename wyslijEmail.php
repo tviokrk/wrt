9ecd08da33705c37275693bf960f7311f9aa3bd7
tviokrk <tviokrk@gmail.com>
2016-06-07 18:59:18 +0200

    Update wyslijEmail.php

--- wyslijEmail.php
+++ wyslijEmail.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<form method="post" action="email.php">
+<form method="post" action="wyslijEmail.php">
   Email: <input name="email" id="email" type="text" /><br />
 
   Message:<br />
@@ -19,19 +19,19 @@
   require_once('class.phpmailer.php');    //dodanie klasy phpmailer
     require_once('class.smtp.php');    //dodanie klasy smtp
     $mail = new PHPMailer();    //utworzenie nowej klasy phpmailer
-    $mail->From = "j.nowak@webio.pl";    //adres e-mail użyty do wysyłania wiadomości
+    $mail->From = "michal.wrt";    //adres e-mail użyty do wysyłania wiadomości
     $mail->FromName = "Jan Nowak";    //imię i nazwisko lub nazwa użyta do wysyłania wiadomości
-    $mail->AddReplyTo('nadawca@domena.pl', 'mailing'); //adres e-mail nadawcy oraz jego nazwa
+    //$mail->AddReplyTo('nadawca@domena.pl', 'mailing'); //adres e-mail nadawcy oraz jego nazwa
                                                  //w polu "Odpowiedz do"  
-    $mail->Host = "smtp.webio.pl";    //adres serwera SMTP wysyłającego e-mail
+    $mail->Host = "smtp.gmail.com";    //adres serwera SMTP wysyłającego e-mail
     $mail->Mailer = "smtp";    //do wysłania zostanie użyty serwer SMTP
     $mail->SMTPAuth = true;    //włączenie autoryzacji do serwera SMTP
-    $mail->Username = "j.nowak@webio.pl";    //nazwa użytkownika do skrzynki e-mail
-    $mail->Password = "hasło";    //hasło użytkownika do skrzynki e-mail
+    $mail->Username = "michal.wrt123@gmail.com";    //nazwa użytkownika do skrzynki e-mail
+    $mail->Password = "michal1234567";    //hasło użytkownika do skrzynki e-mail
     $mail->Port = 587; //port serwera SMTP zależny od konfiguracji dostawcy usługi poczty
     $mail->Subject = "temat";    //Temat wiadomości, można stosować zmienne i znaczniki HTML
-    $mail->Body = 'treść';    //Treść wiadomości, można stosować zmienne i znaczniki HTML     
-    $mail->AddAddress ("biuro@webio.pl","Biuro Webio");    //adres skrzynki e-mail oraz nazwa
+    $mail->Body = $message;    //Treść wiadomości, można stosować zmienne i znaczniki HTML     
+    $mail->AddAddress ( $email);    //adres skrzynki e-mail oraz nazwa
                                                     //adresata, do którego trafi wiadomość
      if($mail->Send())    //sprawdzenie wysłania, jeśli wiadomość została pomyślnie wysłana
         {                      
