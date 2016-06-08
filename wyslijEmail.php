018141ea5c296cbfc5bb1c7356ca14b9afb0dc42
tviokrk <tviokrk@gmail.com>
2016-06-08 21:32:31 +0200

    Update wyslijEmail.php

--- wyslijEmail.php
+++ wyslijEmail.php
@@ -50,7 +50,8 @@ $mail->Subject = 'PHPMailer GMail SMTP test';
 //convert HTML into a basic plain-text alternative body
 //$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
 //Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
+//$mail->AltBody = 'This is a plain-text message body';
+$mail->Body = 'This is a plain-text message body'
 //Attach an image file
 //$mail->addAttachment('images/phpmailer_mini.png');
 //send the message, check for errors
