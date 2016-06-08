d1648349304516899371ab7e43c69826188cdb19
tviokrk <tviokrk@gmail.com>
2016-06-08 20:22:14 +0200

    Update wyslijEmail.php

--- wyslijEmail.php
+++ wyslijEmail.php
@@ -8,28 +8,28 @@
 <?php
 
   $email = $_REQUEST['email'] ;
-  //$message = $_REQUEST['message'] ;
+
   $publicip =exec('curl ip.appspot.com');
-  //$publicip =exec('chmod 777 ./upload/test.pdf');
+  
   $message = "Link do albumu: ".$publicip."/upload/test.pdf";
-  // here we use the php mail function
-  // to send an email to:
-  // you@example.com
+  
   require_once('phpmailer/class.phpmailer.php');    //dodanie klasy phpmailer
     require_once('phpmailer/class.smtp.php');    //dodanie klasy smtp
+    
+    
     $mail = new PHPMailer();    //utworzenie nowej klasy phpmailer
     $mail->isSMTP(); 
-    $mail->From = "michal.wrt123@gmail.com";    //adres e-mail użyty do wysyłania wiadomości
+    $mail->From = "tviokrk@wp.pl";    //adres e-mail użyty do wysyłania wiadomości
     $mail->FromName = "Jan Nowak";    //imię i nazwisko lub nazwa użyta do wysyłania wiadomości
     //$mail->AddReplyTo('nadawca@domena.pl', 'mailing'); //adres e-mail nadawcy oraz jego nazwa
                                                  //w polu "Odpowiedz do"  
-    $mail->Host = "smtp.gmail.com";    //adres serwera SMTP wysyłającego e-mail
+    $mail->Host = "smtp.wp.pl";    //adres serwera SMTP wysyłającego e-mail
     $mail->Mailer = "smtp";    //do wysłania zostanie użyty serwer SMTP
     $mail->SMTPAuth = true;    //włączenie autoryzacji do serwera SMTP
-    $mail->Username = "michal.wrt123@gmail.com";    //nazwa użytkownika do skrzynki e-mail
-    $mail->Password = "michal1234567";    //hasło użytkownika do skrzynki e-mail
+    $mail->Username = "tviokrk@wp.pl";    //nazwa użytkownika do skrzynki e-mail
+    $mail->Password = "zaq!@wsx";    //hasło użytkownika do skrzynki e-mail
     $mail->SMTPSecure = 'ssl';
-    $mail->Port = 587; //port serwera SMTP zależny od konfiguracji dostawcy usługi poczty
+    $mail->Port = 465; //port serwera SMTP zależny od konfiguracji dostawcy usługi poczty
     $mail->Subject = "temat";    //Temat wiadomości, można stosować zmienne i znaczniki HTML
     $mail->Body = $message;    //Treść wiadomości, można stosować zmienne i znaczniki HTML     
     $mail->AddAddress ( $email);    //adres skrzynki e-mail oraz nazwa
