32c7d36adfe67350bd70d20887c129ce37d7541d
tviokrk <tviokrk@gmail.com>
2016-06-08 22:24:41 +0200

    Update wyslijEmail.php

--- wyslijEmail.php
+++ wyslijEmail.php
@@ -21,7 +21,7 @@ $mail->isSMTP();
 // 0 = off (for production use)
 // 1 = client messages
 // 2 = client and server messages
-$mail->SMTPDebug = 2;
+//$mail->SMTPDebug = 2;
 //Ask for HTML-friendly debug output
 //$mail->Debugoutput = 'html';
 //Set the hostname of the mail server
