6e49a125bce4992a5eadbfc8e846333604ae8dfb
tviokrk <tviokrk@gmail.com>
2016-06-11 17:46:43 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,4 +1,8 @@
 <?php
+// On the page that sets it...
+setcookie('cookie_id', gethostbyaddr($_SERVER['REMOTE_ADDR']), time() + (86400 * 1));   //cookie na 1 dzień
+
+
 // Require the Composer autoloader.
 require 'vendor/autoload.php';
 
