d4da265a0eb1803ca292c48c0df23dae25139435
tviokrk <tviokrk@gmail.com>
2016-06-11 18:20:41 +0200

    Update ustawienia.php

--- ustawienia.php
+++ ustawienia.php
@@ -8,7 +8,9 @@
     	<form action="generate_PDF_from_SQS.php" method="post">
     	<input type="submit" name="submit" value="Rozpocnznij proces generowania PDF" />
 	</form>
-		<center><br><h5>Wprowadź nazwę albumu, który chcesz usunąć</h5>
+		<center><br><h5>Wprowadź nazwę albumu, który chcesz usunąć<br><?php
+echo " ".$_COOKIE['cookie_id'];
+?></h5>
         <form action="usun_plik.php" method="post">
     		Nazwa albumu:<input type="text" name="plik_z_albumem" /><br />
     	<input type="submit" name="submit" value="Usuń!" />
