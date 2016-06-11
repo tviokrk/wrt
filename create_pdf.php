182139a099c02ee34c34c64c15233f4f1f819ebb
tviokrk <tviokrk@gmail.com>
2016-06-11 13:41:32 +0200

    Update create_pdf.php

--- create_pdf.php
+++ create_pdf.php
@@ -7,7 +7,7 @@ use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
 use Aws\Sqs\SqsClient;
 
-$email = $_REQUEST['email'] ;
+$email = $_REQUEST['email'];
 //////////////////////////////SQS SENDIND MESSAGES////////////////////////
 $url = 'https://sqs.eu-central-1.amazonaws.com/881078108084/michalo-album';
 $client = SqsClient::factory(array(
@@ -59,37 +59,13 @@ try {
 
 unlink('/var/www/html/upload/test.pdf');
 $filename="/var/www/html/upload/test.pdf";
-//$pdf_file_contents = $pdf->Output("","S");
+
 echo "Robię PDF\n";
 $pdf->Output($filename,'F');
 echo "DONE!\n";
-//$publicip =exec('curl -s icanhazip.com');
-//echo "<a href='http://".$publicip."/upload/test.pdf'>Link do albumu</a>";
 
 //////////////////WYSYŁAM EMAIL/////////////////
-echo "Wysyłam e-mail\n";
-$publicip =exec('curl -s icanhazip.com');
-$message = "Link do albumu: ".$publicip."/upload/test.pdf";
 
-$mail = new PHPMailer;
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = 587;
-$mail->SMTPSecure = 'tls';
-$mail->SMTPAuth = true;    //Whether to use SMTP authentication
-$mail->Username = "michal.wrt123@gmail.com";   //Username to use for SMTP authentication - use full email address for gmail
-$mail->Password = "michal1234567";  //Password to use for SMTP authentication
-$mail->setFrom('michal.wrt123@gmail.com', 'Nadawca');  //Set who the message is to be sent from
-$mail->addAddress($email, 'Odbiorca');  //Set an alternative reply-to address
-$mail->Subject = 'Album Amazon S3';  //Set the subject line
-$mail->Body = $message;  
-$mail->addAttachment('upload/test.pdf');  //Attach an image file
-//send the message, check for errors
-if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message sent!";
-}
 
 ////////////////SKONCZYŁEM WYSYŁKĘ///////////////
 
