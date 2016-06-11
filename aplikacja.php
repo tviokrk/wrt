c633f74be65c77bfadd54f5e3e56278a6610c280
tviokrk <tviokrk@gmail.com>
2016-06-11 22:53:16 +0200

    Update aplikacja.php

--- aplikacja.php
+++ aplikacja.php
@@ -26,7 +26,7 @@ background-color: #99CCFF;
  <td width="25%"><center><a href="upload.html" target="ramka"><img src="ikony/pobierz.png" width="80" height="80"></a><br>Wyślij obrazki</center>
  <td width="25%"><center><a href="gallery.php" target="ramka"><img src="ikony/wyslij.png" width="80" height="80"></a><br>Pokaż galerię</center>
  <td width="25%"><center><a href="order_gallery.html" target="ramka"><img src="ikony/transformuj.png" width="80" height="80"></a><br>Wygeneruj album do PDF</center>
- <td width="25%"><center><a href="index.html" target="ramka"><img src="ikony/powrot.png" width="80" height="80"></a><br>Wyloguj się...</center>
+ <td width="25%"><center><a href="index.html"<img src="ikony/powrot.png" width="80" height="80"></a><br>Wyloguj się...</center>
 <tr>
  <td colspan=6><iframe src="glowna.php" name="ramka" width="100%" height="700" frameborder="0"></iframe>
  <tr>
