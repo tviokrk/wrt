7360322e0832e7002e03cfbdccf28385ce2391ab
tviokrk <tviokrk@gmail.com>
2016-06-11 14:12:37 +0200

    Update create_pdf.php

--- create_pdf.php
+++ create_pdf.php
@@ -3,6 +3,7 @@ date_default_timezone_set('Europe/Warsaw');
 require 'phpmailer/PHPMailerAutoload.php';
 require 'vendor/autoload.php'; // Include the AWS SDK using the Composer autoloader.
 require ("fpdf/fpdf.php");
+
 use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
 use Aws\Sqs\SqsClient;
@@ -65,29 +66,7 @@ $pdf->Output($filename,'F');
 echo "DONE!\n";
 
 //////////////////WYSYŁAM EMAIL/////////////////
-echo "Wysyłam e-mail\n";
-$publicip =exec('curl -s icanhazip.com');
-$message = "Link do albumu: ".$publicip."/upload/test.pdf";
-
-$mail = new PHPMailer;
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = 587;
-$mail->SMTPSecure = 'tls';
-$mail->SMTPAuth = true;    //Whether to use SMTP authentication
-$mail->Username = "michal.wrt123@gmail.com";   //Username to use for SMTP authentication - use full email address for gmail
-$mail->Password = "michal1234567";  //Password to use for SMTP authentication
-$mail->setFrom('michal.wrt123@gmail.com', 'Nadawca');  //Set who the message is to be sent from
-$mail->addAddress($msg['Body'], 'Odbiorca');  //Set an alternative reply-to address
-$mail->Subject = 'Album Amazon S3';  //Set the subject line
-$mail->Body = $message;  
-$mail->addAttachment('upload/test.pdf');  //Attach an image file
-//send the message, check for errors
-if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message sent!";
-}
+include 'wyslijEmail.php';
 ////////////////SKONCZYŁEM WYSYŁKĘ///////////////
 
 ////////////////////////////////////////////////
