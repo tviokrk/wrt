a2bd37b1d11a7d0121810ce3eea2d22e86b61982
tviokrk <tviokrk@gmail.com>
2016-06-11 17:06:28 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -12,7 +12,7 @@ $s3 = new S3Client([
 $file = "./upload/lista";
 //unlink($file);
 // A list of permitted file extensions
-$allowed = array('png', 'jpg', 'gif','zip');
+$allowed = array('png', 'jpg', 'gif');
 if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
 	$extension = pathinfo($_FILES['upl']['name'], PATHINFO_EXTENSION);
 
@@ -22,15 +22,15 @@ if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
 		exit;
 	}
 
-	if(move_uploaded_file($_FILES['upl']['tmp_name'], './upload/'.$_FILES['upl']['name'])){
+	if(move_uploaded_file($_FILES['upl']['tmp_name'], './upload/'.$_POST['identyfikator'].$_FILES['upl']['name'])){
 		echo 'SUKCES';
 		echo '{"status":"success"}';
 		
 			try {
 				   $result = $s3->putObject([
 				        'Bucket' => '160689-michalo',
-				        'Key'    => 'album_'.$_FILES['upl']['name'],
-				        'Body'   => fopen('./upload/'.$_FILES['upl']['name'], 'r'),
+				        'Key'    => 'album_'.$_POST['identyfikator'].$_FILES['upl']['name'],
+				        'Body'   => fopen('./upload/'.$_POST['identyfikator'].$_FILES['upl']['name'], 'r'),
 				        'ACL'    => 'public-read',
 				    ]);
 				    $dane = $result['ObjectURL'];
