390d7a08a91e7e03f1755183f828221a095b4b06
tviokrk <tviokrk@gmail.com>
2016-06-11 13:52:08 +0200

    Update create_pdf.php

--- create_pdf.php
+++ create_pdf.php
@@ -65,8 +65,29 @@ $pdf->Output($filename,'F');
 echo "DONE!\n";
 
 //////////////////WYSYŁAM EMAIL/////////////////
+echo "Wysyłam e-mail\n";
+$publicip =exec('curl -s icanhazip.com');
+$message = "Link do albumu: ".$publicip."/upload/test.pdf";
 
-
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;    //Whether to use SMTP authentication
+$mail->Username = "michal.wrt123@gmail.com";   //Username to use for SMTP authentication - use full email address for gmail
+$mail->Password = "michal1234567";  //Password to use for SMTP authentication
+$mail->setFrom('michal.wrt123@gmail.com', 'Nadawca');  //Set who the message is to be sent from
+$mail->addAddress($msg['Body'], 'Odbiorca');  //Set an alternative reply-to address
+$mail->Subject = 'Album Amazon S3';  //Set the subject line
+$mail->Body = $message;  
+$mail->addAttachment('upload/test.pdf');  //Attach an image file
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
 ////////////////SKONCZYŁEM WYSYŁKĘ///////////////
 
 ////////////////////////////////////////////////
