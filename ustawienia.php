e96e1f941a6582491ae54fc0cfa7422f69309578
tviokrk <tviokrk@gmail.com>
2016-06-11 16:35:38 +0200

    Update ustawienia.php

--- ustawienia.php
+++ ustawienia.php
@@ -5,11 +5,16 @@
         <meta charset="utf-8">
     </head>
     <body>
+    	<form action="create_pdf.php" method="post">
+    	<input type="submit" name="submit" value="Rozpocnznij proces generowania PDF" />
+	</form>
 		<center><br><h5>Wprowadź nazwę albumu, który chcesz usunąć</h5>
         <form action="usun_plik.php" method="post">
     		Nazwa albumu:<input type="text" name="plik_z_albumem" /><br />
     	<input type="submit" name="submit" value="Usuń!" />
 	</form>
+
+	
 <br>
 </center>
     </body>
