6f89a7773b8d37e4fdb767753f853500af4c8759
tviokrk <tviokrk@gmail.com>
2016-06-11 16:38:46 +0200

    Update generate_PDF_from_SQS.php

--- generate_PDF_from_SQS.php
+++ generate_PDF_from_SQS.php
@@ -8,17 +8,11 @@ use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
 use Aws\Sqs\SqsClient;
 
-$email = $_REQUEST['email'];
-//////////////////////////////SQS SENDIND MESSAGES////////////////////////
-$url = 'https://sqs.eu-central-1.amazonaws.com/881078108084/michalo-album';
+
 $client = SqsClient::factory(array(
     //'profile' => '<profile in your aws credentials file>'
     'version' => 'latest',
     'region'  => 'eu-central-1'
-));
-$client->sendMessage(array(
-    'QueueUrl'    => $url,
-    'MessageBody' => $email,
 ));
 //////////////////////////////SQS RECEVING MESSAGES////////////////////////
 
