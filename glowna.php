0bce10aab242622bd028c61397e66f7378bc60e1
tviokrk <tviokrk@gmail.com>
2016-06-11 18:20:16 +0200

    Update glowna.php

--- glowna.php
+++ glowna.php
@@ -1,7 +1,1 @@
-<center><br><br><br><br><br><br>
-
-<h1>Witaj 
-<?php
-echo " ".$_COOKIE['cookie_id'];
-?>
-!</h1></center>
+<center><br><br><br><br><br><br><h1>Witaj w Gallery Creator!</h1></center>
