a381133de1642bd76f944f2c3d95ed121c6894bc
tviokrk <tviokrk@gmail.com>
2016-06-11 22:02:23 +0200

    Create ustaw_cookie.php

---
+++ ustaw_cookie.php
@@ -0,0 +1,6 @@
+<?php
+// On the page that sets it...
+$id_value=hash('ripemd160', $email);   //hash ip usera
+setcookie('cookie_id', $id_value, time() + (86400 * 1));   //cookie na 1 dzień
+setcookie('email', $email, time() + (86400 * 1));   //cookie na 1 dzień
+?>
