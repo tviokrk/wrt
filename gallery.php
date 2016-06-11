8acd8b4dfebbfea7c3c5cae8d7c895185bee6f79
tviokrk <tviokrk@gmail.com>
2016-06-11 18:28:38 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -3,7 +3,7 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl">
 <head>
-	<title>Prosty skrypt galerii zdjęć z efektem Highslide</title>
+	<title>Gotowy skrypt do galerii</title>
 	<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=UTF-8" />
 	<script type="text/javascript" src="javascript/highslide-with-gallery.js"></script>
 	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
@@ -13,9 +13,7 @@
 	hs.transitions = ['expand', 'crossfade'];
 	hs.outlineType = 'rounded-white';
 	hs.fadeInOut = true;
-	//hs.dimmingOpacity = 0.75;
 
-	// Add the controlbar
 	if (hs.addSlideshow) hs.addSlideshow({
 		//slideshowGroup: 'group1',
 		interval: 5000,
@@ -36,11 +34,10 @@
 <div id="galeria">
 <ul>
 <?php
-$file_name = "./upload/".$_COOKIE['cookie_id'];
-$link = file($file_name);
-for ($i=0; $i<count($link); $i++)
+$file_name = "./upload/".$_COOKIE['cookie_id']; 
+$link = file($file_name);  //czytam z pliku cookie_id w lokalizacji ./upload/    , są tam adresy www na s3 do zdjęć
+for ($i=0; $i<count($link); $i++)     //dla wszystkich wierszy z linkami tworzę galerię
 {
- //echo $link;
  echo '<li><a href="'.$link[$i].'" class="highslide" onclick="return hs.expand(this)"><img width="200" height="133" src="'.$link[$i].'" /></a></li>';
 }
 
