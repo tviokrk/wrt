7610092a56d7c95b7123d19c54591b2ef12d0c75
tviokrk <tviokrk@gmail.com>
2016-06-11 22:39:44 +0200

    Update ustaw_cookie.php

--- ustaw_cookie.php
+++ ustaw_cookie.php
@@ -1,7 +1,7 @@
 <?php
-// On the page that sets it...
+// Ustawiam cookie żeby pliki były dla każdej osoby osobne
 $id_value=hash('ripemd160', $_POST['email']);   //hash ip usera
-setcookie('cookie_id', $id_value, time() + (86400 * 1));   //cookie na 1 dzień
-setcookie('email', $_POST['email'], time() + (86400 * 1));   //cookie na 1 dzień
+setcookie('cookie_id', $id_value, time() + (86400 * 1));   //cookie na 1 dzień    //cookie jako nazwa dla plików tzw. PREFIX
+setcookie('email', $_POST['email'], time() + (86400 * 1));   //cookie na 1 dzień   //cookie ustawiane na podstawie formularza logowania
 header("Location: aplikacja.php");
 ?>
