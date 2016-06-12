9910245cf45d31371ef845149341fba9b183a677
tviokrk <tviokrk@gmail.com>
2016-06-12 23:22:13 +0200

    Update ustaw_cookie.php

--- ustaw_cookie.php
+++ ustaw_cookie.php
@@ -3,6 +3,5 @@
 $id_value=hash('ripemd160', $_POST['email']);   //hash ip usera
 setcookie('cookie_id', $id_value, time() + (86400 * 1));   //cookie na 1 dzień    //cookie jako nazwa dla plików tzw. PREFIX
 setcookie('email', $_POST['email'], time() + (86400 * 1));   //cookie na 1 dzień   //cookie ustawiane na podstawie formularza logowania
-setcookie('imie', $_POST['imie'], time() + (86400 * 1));   //cookie na 1 dzień   //cookie ustawiane na podstawie formularza logowania, imie do tresci maila 4Fun
 header("Location: aplikacja.php");
 ?>
